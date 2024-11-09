59e25f6eddeb1c43d63b1153e29968f491ff036c
정지연 <jiyeonyh@jeongjiyeon-ui-MacBookAir.local>
2024-11-09 21:06:28 +0900

    init

---
+++ src/main/kotlin/com/winter/university/UniversityApplication.kt
@@ -0,0 +1,11 @@
+package com.winter.university
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UniversityApplication
+
+fun main(args: Array<String>) {
+	runApplication<UniversityApplication>(*args)
+}
---
+++ src/test/kotlin/com/winter/university/UniversityApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.winter.university
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UniversityApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
